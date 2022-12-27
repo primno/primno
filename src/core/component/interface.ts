cb3d431f977a37955db00b94db6166240df3103b
Xavier Monin <xaviermonin@hotmail.com>
2022-12-27 14:58:02 +0100

    Add comment

--- src/core/component/interface.ts
+++ src/core/component/interface.ts
@@ -5,6 +5,9 @@
     input: any;
 }
 
+/**
+ * Inherit that interface in component with output.
+ */
 export interface OnInit {
     mnOnInit(): void;
 }
@@ -13,6 +16,9 @@ export interface OnDestroy {
     mnOnDestroy(): void;
 }
 
+/**
+ * Inherit that interface in component with config.
+ */
 export interface Config {
     config: any;
 }
